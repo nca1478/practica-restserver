2028316fe0516cc989abfae244c3622b26c60443
Nelson Cadenas <nca1478@yahoo.com>
2018-08-06 17:11:04 -0400

    Variable de entorno Mongo

--- server/config/config.js
+++ server/config/config.js
@@ -12,7 +12,7 @@ if( process.env.NODE_ENV === 'dev' ){
 	urlDB = 'mongodb://localhost:27017/cafe-pract';
 }
 else{
-	urlDB = 'mongodb://cafe-user:14006016nca@ds113942.mlab.com:13942/cafe-pract';
+	urlDB = process.env.MONGO_URI;
 }
 
 process.env.URL_DB = urlDB;
